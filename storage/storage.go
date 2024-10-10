e055ba9fdbf5decc4b4d291de5fa08d814a0b3fd
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-10-10 14:14:24 +0500

    Ready

--- storage/storage.go
+++ storage/storage.go
@@ -1,9 +1,28 @@
 package storage
 
 import (
+	"checker/storage/postgres"
 	"checker/storage/repo"
+	"database/sql"
+	"log/slog"
 )
 
-type Isorage interface {
+type Istorage interface {
 	Check() repo.ICheckStorage
+}
+
+type StoragePro struct {
+	DB     *sql.DB
+	Logger *slog.Logger
+}
+
+func NewStoragePro(DB *sql.DB, logger *slog.Logger) Istorage {
+	return &StoragePro{
+		DB:     DB,
+		Logger: logger,
+	}
+}
+
+func (pro *StoragePro) Check() repo.ICheckStorage {
+	return postgres.NewCheckRepo(pro.DB)
 }
